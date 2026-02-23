232f49ed02324711a11f0fd8c42d6a30e8ae940c
NukhbaAhmad <nukhbaahmadali@gmail.com>
2026-02-23 15:39:15 +0500

    Fix/file name conventions

--- src/models/plugins/index.js
+++ src/models/plugins/index.js
@@ -1,4 +1,4 @@
-const { toJSON } = require("./toJSON.plugin");
 const paginate = require("./paginate.plugin");
+const { toJSON } = require("./toJson.plugin");
 const doesIdExists = require("./doesIdExists.plugin");
 module.exports = { paginate, toJSON, doesIdExists };
